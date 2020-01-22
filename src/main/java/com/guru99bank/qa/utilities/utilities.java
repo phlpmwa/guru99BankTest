0390d2a9dace0b0019abb1f409a1162467c455f0
phlpmwa <mwaphiwa@gmail.com>
2020-01-22 03:18:57 -0800

    added utility class

---
+++ src/main/java/com/guru99bank/qa/utilities/utilities.java
@@ -0,0 +1,9 @@
+package com.guru99bank.qa.utilities;
+
+public class utilities {
+	public static long PAGE_LOAD_TIMEOUT=20;
+	public static long IMPLICIT_WAIT=10;
+	
+	public static String expectedLoginTitle="Guru99 Bank Home Page";
+
+}
