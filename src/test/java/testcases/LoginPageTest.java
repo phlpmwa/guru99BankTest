1c1459a0bd9616f2ec01eeaf50cd7da976cfefb7
phlpmwa <mwaphiwa@gmail.com>
2020-01-23 00:36:55 -0800

    working with correct and wrong credentials

--- src/test/java/testcases/LoginPageTest.java
+++ src/test/java/testcases/LoginPageTest.java
@@ -3,6 +3,7 @@ package testcases;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import com.guru99bank.qa.base.TestBase;
@@ -11,43 +12,69 @@ import com.guru99bank.qa.pages.LoginPage;
 import com.guru99bank.qa.utilities.utilities;
 
 public class LoginPageTest extends TestBase {
-	
+
 	LoginPage loginPage;
 	HomePage homePage;
-	public LoginPageTest()
-	{
+
+	public LoginPageTest() {
 		super();
 	}
-	
-	
+
 	@BeforeMethod
 	public void setUp() {
 		initialization();
 		loginPage = new LoginPage();
 
 	}
+
 	@Test(priority = 1)
-	
-	public void verifyLoginPageTitle()
-	{
-		String actualLoginTitle=loginPage.validateLoginPageTitle();
-		Assert.assertEquals(actualLoginTitle,utilities.expectedLoginTitle
-				);
+	@Ignore
+
+	public void verifyLoginPageTitle() {
+		String actualLoginTitle = loginPage.validateLoginPageTitle();
+		Assert.assertEquals(actualLoginTitle, utilities.expectedLoginTitle);
 	}
-	
+
 	@Test(priority = 2)
-		public void login()
-	{
-		homePage=loginPage.Login(prop.getProperty("UserID"),prop.getProperty("password"));
+	@Ignore
+	public void login() {
+		homePage = loginPage.Login(prop.getProperty("UserID"), prop.getProperty("password"));
 	}
-	
+
 	@Test(priority = 3)
-		public void reset()
-	{
-		loginPage=loginPage.reset(prop.getProperty("UserID"),prop.getProperty("password"));
+	@Ignore
+	public void reset() {
+		loginPage = loginPage.reset(prop.getProperty("UserID"), prop.getProperty("password"));
 	}
+
+	@Test
+	@Ignore
+
+	public void InvalidUserId() {
+		homePage = loginPage.Login(prop.getProperty("UserIDW"), prop.getProperty("password"));
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+	}
+
+	@Test
+	@Ignore
+
+	public void InvalidPassword() {
+		homePage = loginPage.Login(prop.getProperty("UserID"), prop.getProperty("passwordW"));
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+	}
+
+	@Test
+
+	public void InvalidCredentials() {
+		homePage = loginPage.Login(prop.getProperty("UserIDW"), prop.getProperty("passwordW"));
+		System.out.println(driver.switchTo().alert().getText());
+		driver.switchTo().alert().dismiss();
+	}
+
 	@AfterMethod
 	public void tearDown() {
-		driver.quit();
+		// driver.quit();
 	}
 }
